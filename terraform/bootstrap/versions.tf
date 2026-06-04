70e1a98d906a32bd3764ed22af580ad3b3d8c845
Dmytro Kabachenko <dimakaba4enko@gmail.com>
2026-06-04 17:36:10 +0300

    feat: EPMCACMAWS-451: Remove null_resource to sync tfvars

--- terraform/bootstrap/versions.tf
+++ terraform/bootstrap/versions.tf
@@ -7,7 +7,11 @@ terraform {
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.2"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
 }
