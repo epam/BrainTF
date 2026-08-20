c105dcc165aec9127d1888526e5053fe40d5d411
Dmytro P <dmytro_pichuiev@epam.com>
2026-08-20 11:05:51 +0000

    Change layer name

--- terraform/main_module/main.tf
+++ terraform/main_module/main.tf
@@ -345,7 +345,7 @@ module "ai_lambda" {
   log_level            = var.log_level
   artifacts_path       = var.artifacts_path
   tags                 = local.tags
-  layer_name           = "${var.region}-${var.vcs_repo_name}-layer"
+  layer_name           = "layer-${var.region}-${var.vcs_repo_name}"
 }
 
 # ======================= VCS Integration =======================
