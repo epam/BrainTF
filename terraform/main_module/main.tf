111e13da121139451187d1d6ec720f41cc20feb7
Dmytro <dmitrypichuev@gmail.com>
2026-08-25 14:04:03 +0300

    refactor: EPMCACMAWS-492 change lambda layer name to a dynamic one (#25)

--- terraform/main_module/main.tf
+++ terraform/main_module/main.tf
@@ -345,6 +345,7 @@ module "ai_lambda" {
   log_level            = var.log_level
   artifacts_path       = var.artifacts_path
   tags                 = local.tags
+  layer_name           = lower("layer-${var.vcs_repo_name}-${var.region}")
 }
 
 # ======================= VCS Integration =======================
